33d56e2930374aea100f77f936e1e128513cdb36
ebrahim <ebrahim@softbyteit.com>
2022-09-27 14:14:50 +0600

    Bmi Calculater update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:bmi_calculator_project/pages/home_page.dart';
-import 'package:bmi_calculator_project/pages/secondPage.dart';
 import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,28 +10,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        canvasColor: Color(0xffF5F3F4),
-         
-       
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.deepOrange,
       ),
       home: HomePage(),
-      initialRoute: HomePage.routeName ,
-      routes: {
-
-        HomePage.routeName:(context) => HomePage(),
-        SecondPage.routeName: (context) => SecondPage(),
-
-      },
     );
   }
 }
 
- 
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,228 +1,129 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, must_be_immutable, unused_field, prefer_final_fields, sized_box_for_whitespace
 import 'dart:math';
-
-
-import 'package:bmi_calculator_project/pages/secondPage.dart';
+import 'package:bmi_calculator_project/pages/score_page.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:swipeable_button_view/swipeable_button_view.dart';
+import '../widget/age_weight_widget.dart';
+import '../widget/gender_widget.dart';
+import '../widget/height_widget.dart';
 
 class HomePage extends StatefulWidget {
-  static final routeName = "home-page";
-
-  const HomePage({Key? key}) : super(key: key);
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-int tableIndex = 4;
-
 class _HomePageState extends State<HomePage> {
-  double _currentFirstSliderValue = 50;
-  double _currentSecondSliderValue = 1.5;
-  double _bmiResult = 22.22;
-  String _bmiCategory = "Normal range";
+  int _gender = 0;
+  int _height = 150;
+  int _age = 30;
+  int _weight = 50;
+  bool _isFinished = false;
+  double _bmiScore = 0;
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        actions: [PopupMenuButton(itemBuilder: (context) => [])],
+        centerTitle: true,
+        title: Text("BMI Calculator"),
       ),
-      drawer: Drawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Text(
-              "Select your height & Weight!",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  fontSize: 20),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height - 90,
+          padding: EdgeInsets.all(8),
+          child: Card(
+            elevation: 12,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15)
             ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              alignment: Alignment.bottomCenter,
-              height: 450,
-              width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Container(
-                            width: 110,
-                            height: 40,
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                border: Border.all(
-                                    color: Colors.green, width: 1.5)),
-                            child: FittedBox(
-                                child: Text(
-                              "Weight:  ${_currentFirstSliderValue.toStringAsFixed(1)} kg",
-                              style: TextStyle(
-                                  color: Colors.green,
-                                  fontWeight: FontWeight.w500),
-                            ))),
-                        Expanded(
-                          child: RotatedBox(
-                            quarterTurns: -1,
-                            child: Slider(
-                              value: _currentFirstSliderValue,
-                              max: 150,
-                              min: 0,
-                              thumbColor: Color(0xffb300b3),
-                              divisions: 150,
-                              label:
-                                  _currentFirstSliderValue.toStringAsFixed(1),
-                              onChanged: (double value) {
-                                setState(() {
-                                  _currentFirstSliderValue = value;
-                                  getBMIResult(_currentFirstSliderValue,
-                                      _currentSecondSliderValue);
-                                });
-                              },
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Image.asset(
-                      "assets/images/man.jpg",
-                      height: 350,
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        Container(
-                            width: 110,
-                            height: 40,
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                border: Border.all(
-                                    color: Colors.green, width: 1.5)),
-                            child: FittedBox(
-                                child: Text(
-                              "Height:  ${_currentSecondSliderValue.toStringAsFixed(2)} m",
-                              style: TextStyle(
-                                  color: Colors.green,
-                                  fontWeight: FontWeight.w500),
-                            ))),
-                        Expanded(
-                          child: RotatedBox(
-                            quarterTurns: -1,
-                            child: Slider(
-                              thumbColor: Color(0xffb300b3),
-                              value: _currentSecondSliderValue,
-                              max: 2.2,
-                              min: 1.2,
-                              divisions: 1000,
-                              label:
-                                  _currentSecondSliderValue.toStringAsFixed(1),
-                              onChanged: (double value) {
-                                setState(() {
-                                  _currentSecondSliderValue = value;
-                                  getBMIResult(_currentFirstSliderValue,
-                                      _currentSecondSliderValue);
-                                });
-                              },
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Text(
-              "Your BMI: ${_bmiResult.toStringAsFixed(1)}",
-              style: TextStyle(
-                  color: Colors.red,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 22,
-                  letterSpacing: 1,
-                  wordSpacing: 1),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              _bmiCategory,
-              style: TextStyle(
-                  color: Colors.green,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 18,
-                  letterSpacing: 1,
-                  wordSpacing: 1),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Container(
-                height: 40,
-                width: 200,
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(SecondPage.routeName,
-                          arguments: _bmiResult);
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: GenderWidget(
+                    onChange: (genderValue) {
+                      _gender = genderValue;
                     },
-                    child: Text(
-                      "View Details",
-                      style: TextStyle(fontSize: 18),
-                    )))
-          ],
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: HeightWidget(
+                    onChange: (sliderValue) {
+                      _height = sliderValue;
+                    },
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      AgeWidget(
+                        onChange: (ageValue) {
+                          _age = ageValue;
+                        },
+                        title: 'Age',
+                        initialValue: 30,
+                        min: 0,
+                        max: 120,
+                      ),
+                      AgeWidget(
+                        onChange: (weightValue) {
+                          _weight = weightValue;
+                        },
+                        title: 'Weight',
+                        initialValue: 50,
+                        min: 0,
+                        max: 200,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 60,
+                    vertical: 20,
+                  ),
+                  child: SwipeableButtonView(
+                      isFinished: _isFinished,
+                      onFinish: () async {
+                        await Navigator.push(
+                            context,
+                            PageTransition(
+                                child:
+                                    ScorePage(bmiScore: _bmiScore, age: _age),
+                                type: PageTransitionType.fade));
+                        setState(() {
+                          _isFinished = false;
+                        });
+                      },
+                      onWaitingProcess: () {
+                        calculateBmi();
+                        Future.delayed(Duration(seconds: 1), () {
+                          setState(() {
+                            _isFinished = true;
+                          });
+                        });
+                      },
+                      activeColor: Colors.deepOrange,
+                      buttonWidget: Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: Colors.black,
+                      ),
+                      buttonText: 'CALCULATE'),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 
-  void getBMIResult(double _height, double _weight) {
-    setState(() {
-      num powOfWeight = pow(_weight, 2);
-
-      _bmiResult = _height / powOfWeight;
-      getBMICategory(_bmiResult);
-    });
-  }
-
-  void getBMICategory(double bmiResult) {
-    setState(() {
-      if (bmiResult < 16.0) {
-        _bmiCategory = "Underweight (Severe thinness)";
-        tableIndex = 1;
-      } else if (bmiResult >= 16.0 && bmiResult <= 16.9) {
-        _bmiCategory = "Underweight (Moderate thinness)";
-        tableIndex =2;
-      } else if (bmiResult >= 17.0 && bmiResult <= 18.4) {
-        _bmiCategory = "Underweight (Mild thinness)";
-        tableIndex = 3;
-      } else if (bmiResult >= 18.5 && bmiResult <= 24.9) {
-        _bmiCategory = "Normal range";
-        tableIndex = 4;
-      } else if (bmiResult >= 25.0 && bmiResult <= 29.9) {
-        _bmiCategory = "Overweight (Pre-obese)";
-        tableIndex =5;
-      } else if (bmiResult >= 30.0 && bmiResult <= 34.9) {
-        _bmiCategory = "Obese (Class I)";
-        tableIndex = 6;
-      } else if (bmiResult >= 35.0 && bmiResult <= 39.9) {
-        _bmiCategory = "Obese (Class II)";
-        tableIndex =7;
-      } else if (bmiResult >= 40.0) {
-        _bmiCategory = "Obese (Class III)";
-        tableIndex =8;
-      }
-    });
+  void calculateBmi() {
+    _bmiScore = _weight / pow(_height / 100, 2);
   }
 }
